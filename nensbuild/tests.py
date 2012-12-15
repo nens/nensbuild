c469deb22962a4eb446dd91d4654e63f8a2c0cc8
Roland van Laar <roland@micite.net>
2012-12-15 23:57:06 +0100

    Rewrote tests to use setUp code.
    This test code uses the setUp function instead of the
    previous run_func. Run_func needed a boolean argument as a return
    call, this return wasn't always needed but needed to be specified
    because of the way mock.patch works.

--- nensbuild/tests.py
+++ nensbuild/tests.py
@@ -9,38 +9,43 @@ from nensbuild import build
 
 class TestBuild(unittest.TestCase):
 
-    @mock.patch('subprocess.call')
-    def run_func(self, func, exists_return_value, subprocess_call):
-        with mock.patch('os.path.exists',
-                        return_value=exists_return_value):
-            func()
-        return subprocess_call
+    def setUp(self):
+        patcher1 = mock.patch('subprocess.call')
+        patcher2 = mock.patch('os.path.exists')
+
+        self.call = patcher1.start()
+        self.exists = patcher2.start()
+
+        self.addCleanup(patcher1.stop)
+        self.addCleanup(patcher2.stop)
 
     def test_create_link(self):
-        call = self.run_func(build.link, False)
-        self.assertTrue(call.called)
-        call.assert_called_with(
+        self.exists.return_value = False
+        build.link()
+        self.call.assert_called_with(
             ['ln', '-sf', 'development.cfg', 'buildout.cfg'])
 
     def test_not_create_link(self):
-        call = self.run_func(build.link, True)
-        self.assertFalse(call.called)
+        self.exists.return_value = True
+        build.link()
+        self.assertFalse(self.call.called)
 
     def test_run_boostrap(self):
-        call = self.run_func(build.bootstrap, False)
-        self.assertTrue(call.called)
-        call.assert_called_with(
+        self.exists.return_value = False
+        build.bootstrap()
+        self.call.assert_called_with(
             ['python', 'bootstrap.py'])
 
     def test_not_run_bootstrap(self):
-        call = self.run_func(build.bootstrap, True)
-        self.assertFalse(call.called)
+        self.exists.return_value = True
+        build.bootstrap()
+        self.assertFalse(self.call.called)
 
     def test_run_buildout(self):
-        call = self.run_func(build.buildout, False)
-        self.assertTrue(call.called)
-        call.assert_called_with(['bin/buildout'])
+        build.buildout()
+        self.call.assert_called_with(['bin/buildout'])
 
     def test_run_all(self):
-        call = self.run_func(build.main, False)
-        self.assertEqual(call.call_count, 3)
+        self.exists.return_value = False
+        build.main()
+        self.assertEqual(self.call.call_count, 3)
